a4c54d5a23bd8115fd59aa100467ba12bb940f47
Amayya <105234629+danieleheckel@users.noreply.github.com>
2024-10-02 14:36:33 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,38 +1,71 @@
 const quizData = [
     {
-        question: "O que você faria se estivesse num apocalipse zumbi?",
+        question: "onde você moraria?",
         answers: [
-            { text: "Caçar por diversão", animal: "Leão Zumbi", image: "img/leao.jpg" },
-            { text: "Dormir até tudo acabar", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp" },
-            { text: "Nadar para longe", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
-            { text: "Correr o mais rápido possível", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" }
+            { text: "Nas rochas", animal: "Leão Zumbi", image: "img/leao.jpg" },
+            { text: "Na floresta", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp" },
+            { text: "No oceano", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
+            { text: "Na savana", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
+            { text: "Em cidades abandonadas", animal: "Macaco Zumbi", image: "img/macaco.jpg" },
+            { text: "Em campos", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" },
+            { text: "Nas montanhas", animal: "Águia Zumbi", image: "img/aguia.jpeg" }
         ]
     },
     {
-        question: "O que você comeria no apocalipse zumbi?",
+        question: "O que você come?",
         answers: [
-            { text: "Carne crua", animal: "Leão Zumbi", image: "img/leao.jpg" },
-            { text: "Plantas apodrecidas", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
-            { text: "Peixes infectados", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
-            { text: "Frutas mortas", animal: "Macaco Zumbi", image: "img/macaco.jpg" }
+            { text: "De outros animais, humanos", animal: "Leão Zumbi", image: "img/leao.jpg" },
+            { text: "De folhas das árvores", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp" },
+            { text: "Peixes, restos de embarcações", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
+            { text: "Plantas, frutas e carne fresca", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
+            { text: "Suprimentos de mecados vazios", animal: "Macaco Zumbi", image: "img/macaco.jpg" },
+            { text: "Frutas, feno e animais pequenos", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" },
+            { text: "Outros animais, humanos e suprimentos humanos acessíveis", animal: "Águia Zumbi", image: "img/aguia.jpeg" }
         ]
     },
     {
-        question: "Como você descreveria sua habilidade zumbi?",
+        question: "Qual sua habilidade zumbi?",
         answers: [
-            { text: "Caçador(a) implacável", animal: "Leão Zumbi", image: "img/leao.jpg" },
-            { text: "Poder telepático", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
-            { text: "Força bruta", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
-            { text: "Agilidade e travessuras", animal: "Macaco Zumbi", image: "img/macaco.jpg" }
+            
+            { text: "Ser um dos animais mais fortes e violentos", animal: "Leão Zumbi", image: "img/leao.jpg" },
+            { text: "Se esconder e ter garras enormes e fortes", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp" },
+            { text: "Nadar super rápido, muito esperto", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
+            { text: "Marcar suas vítimas, esmagar tudo por onde anda", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
+            { text: "É muito esperto, consegue usar armas", animal: "Macaco Zumbi", image: "img/macaco.jpg" },
+            { text: "Muito rápido, percorre longas distâncias", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" },
+            { text: "Voa, ter garras afiadas", animal: "Águia Zumbi", image: "img/aguia.jpeg" }
         ]
     },
     {
-        question: "Onde você gostaria de vagar no apocalipse?",
+        question: "O que você faria se vesse um humano?",
         answers: [
-            { text: "Ruínas de uma cidade", animal: "Águia Zumbi", image: "img/aguia.jpeg" },
-            { text: "Florestas sombrias", animal: "Leão Zumbi", image: "img/leao.jpg" },
-            { text: "Oceano infestado", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
-            { text: "Desertos áridos", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" }
+            { text: "Devorar", animal: "Leão Zumbi", image: "img/leao.jpg" },
+            { text: "Abraçar", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp" },
+            { text: "Afogar", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
+            { text: "Esmagar", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
+            { text: "Robar", animal: "Macaco Zumbi", image: "img/macaco.jpg" },
+            { text: "Atropelar", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" },
+            { text: "Despedassar", animal: "Águia Zumbi", image: "img/aguia.jpeg" }
+        ]
+    },
+    {
+        question: "Quantos andam no seu bando (machos e fêmeas)?",
+        answers: [
+            { text: "1 a 12", animal: "Leão Zumbi", image: "img/leao.jpg" },
+            { text: "1 a 2", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp" },
+            { text: "10 a 30", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg" },
+            { text: "8 a 100", animal: "Elefante Zumbi", image: "img/elefante.jpeg" },
+            { text: "1 a 30", animal: "Macaco Zumbi", image: "img/macaco.jpg" },
+            { text: "1 a 10", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" },
+            { text: "Solitário", animal: "Águia Zumbi", image: "img/aguia.jpeg" }
+        ]
+    },
+    {
+        question: "Você seria amigável?",
+        answers: [
+            { text: "Hostil", animal: "Leão Zumbi", image: "img/leao.jpg", animal: "Elefante Zumbi", image: "img/elefante.jpeg",  animal: "Águia Zumbi", image: "img/aguia.jpeg" },
+            { text: "Tranquilo", animal: "Preguiça Zumbi", image: "img/bicho_preguiça.webp", animal: "Cavalo Zumbi", image: "img/cavalo.jpeg" },
+            { text: "Parcial", animal: "Golfinho Zumbi", image: "img/golfinho.jpeg", animal: "Macaco Zumbi", image: "img/macaco.jpg" },
         ]
     }
 ];
